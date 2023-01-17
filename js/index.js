d0aadde69aa46639bf92696186dbad775ecd2c9f
Russell Frankpitt <75209707+rustyff12@users.noreply.github.com>
2023-01-17 12:30:42 +0100

    beginning nav

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// let hamEl = document.getElementById("btn");
+
+// function openNav() {
+//     console.log("Hello");
+// }
+
+// document.getElementById("btn").onclick = function() {
+//     this.classList.toggle("nav-toggle");
+//     this.classList.toggle("nav-toggle-off");
+// }
